ff6d63bb09b7604bcba9cf9d8f827d6061629821
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-03-14 13:18:44 +0700

    Embed pages now use try catch so error can be send to channel. Also added ways to not input emoji everytime cause it's annoying and if i move it to the last then i have to reformat so many commands

--- local_dependencies/functions.js
+++ local_dependencies/functions.js
@@ -47,60 +47,66 @@ module.exports = {
 
     // Paginator scrapped from https://github.com/saanuregh/discord.js-pagination. Modified by me personally
     paginationEmbed: async function(msg, pages, emojiList = ['⏪', '⏩', '❌'], timeout = 120000) {
-        if (!msg && !msg.channel) throw new Error('Channel is inaccessible.');
-        if (!pages) throw new Error('Pages are not given.');
-        if (emojiList.length !== 3) throw new Error('Need two emojis.');
-        let page = 0;
-        const curPage = await msg.channel.send(pages[page].setFooter(`Page ${page + 1} / ${pages.length}`));
-        for (const emoji of emojiList) await curPage.react(emoji);
-        const reactionCollector = curPage.createReactionCollector(
-            (reaction, user) => emojiList.includes(reaction.emoji.name) && !user.bot && user.id === msg.author.id,
-            { time: timeout }
-        );
-        reactionCollector.on('collect', reaction => {
-            reaction.users.remove(msg.author);
-            switch (reaction.emoji.name) {
-                case emojiList[0]:
-                    page = page > 0 ? --page : pages.length - 1;
-                    break;
-                case emojiList[1]:
-                    page = page + 1 < pages.length ? ++page : 0;
-                    break;
-                case emojiList[2]:
-                    curPage.reactions.removeAll(); // Remove Reaction
+        try { // Try
+            if (!msg && !msg.channel) throw new Error('Channel is inaccessible.');
+            if (!pages) throw new Error('Pages are not given.');
+            if (emojiList.length !== 3) emojiList = ['⏪', '⏩', '❌'];
+            let page = 0;
+            const curPage = await msg.channel.send(pages[page].setFooter(`Page ${page + 1} / ${pages.length}`));
+            for (const emoji of emojiList) await curPage.react(emoji);
+            const reactionCollector = curPage.createReactionCollector(
+                (reaction, user) => emojiList.includes(reaction.emoji.name) && !user.bot && user.id === msg.author.id,
+                { time: timeout }
+            );
+            reactionCollector.on('collect', reaction => {
+                reaction.users.remove(msg.author);
+                switch (reaction.emoji.name) {
+                    case emojiList[0]:
+                        page = page > 0 ? --page : pages.length - 1;
+                        break;
+                    case emojiList[1]:
+                        page = page + 1 < pages.length ? ++page : 0;
+                        break;
+                    case emojiList[2]:
+                        curPage.reactions.removeAll(); // Remove Reaction
 
-                    pages[page] // Edit Current page make all empty
-                    .setAuthor(``) // Author
-                    .setTitle(`Embed Viewing Closed by Message Author`) // Title
-                    .setDescription(`❌ ${msg.author} Closed the embed`) // Desc
-                    .setFooter(``) // Footer
-                    .setImage(``) // Image
-                    .setTimestamp(``) // Timestamp
-                    .setThumbnail(``) // Thumbnail
-                    .fields = []; // Field 
-                    
+                        pages[page] // Edit Current page make all empty
+                        .setAuthor(``) // Author
+                        .setTitle(`Embed Viewing Closed by Message Author`) // Title
+                        .setDescription(`❌ ${msg.author} Closed the embed`) // Desc
+                        .setFooter(``) // Footer
+                        .setImage(``) // Image
+                        .setTimestamp(``) // Timestamp
+                        .setThumbnail(``) // Thumbnail
+                        .fields = []; // Field 
+                        
 
-                    curPage.edit(pages[page]); // Edit it
+                        curPage.edit(pages[page]); // Edit it
 
-                    return // So it end there, no error
-                    break;
-                default:
-                    break;
-            }
-            curPage.edit(pages[page].setFooter(`Page ${page + 1} / ${pages.length}`));
-        });
-        reactionCollector.on('end', () => {
-            if (!curPage.deleted) { // If curpage is still there
-                if (pages[page].footer.text !== '') { // If it's not closed by author
-                    curPage.reactions.removeAll()
-
-                    pages[page]
-                    .setFooter(`Page ${page + 1} / ${pages.length} | Pages switching removed due to timeout`);
-    
-                    curPage.edit(pages[page]);
+                        return // So it end there, no error
+                        break;
+                    default:
+                        break;
                 }
-            }
-        });
-        return curPage;
+                curPage.edit(pages[page].setFooter(`Page ${page + 1} / ${pages.length}`));
+            });
+            reactionCollector.on('end', () => {
+                if (!curPage.deleted) { // If curpage is still there
+                    if (pages[page].footer.text !== '') { // If it's not closed by author
+                        curPage.reactions.removeAll()
+
+                        pages[page]
+                        .setFooter(`Page ${page + 1} / ${pages.length} | Pages switching removed due to timeout`);
+        
+                        curPage.edit(pages[page]);
+                    }
+                }
+            });
+            return curPage;
+        } 
+        catch (e) { // Catch error
+            msg.channel.send(`${e}`);
+            throw (`${e}`);
+        }
     }
 };
