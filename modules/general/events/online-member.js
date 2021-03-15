c63a8cbd7236b95d3cac8209b6761f92ff282e02
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-03-15 18:25:05 +0700

    Added more information to the embed

--- modules/general/events/online-member.js
+++ modules/general/events/online-member.js
@@ -1,4 +1,6 @@
 const { MessageEmbed } = require("discord.js");
+const Moment = require('moment-timezone');
+const prettyMS = require('pretty-ms')
 
 module.exports = client => {
     const guild = client.guilds.cache.get('640790707082231834') // Server ID
@@ -7,6 +9,8 @@ module.exports = client => {
         const channelID = '820964768067878922'; // The channel it's in
         const rulesChannelID = '640825665310031882'; // The rules channel
         const modsRolesID = '645494894613233665'; // Mod roles ID
+        var today = Moment().tz('Asia/Jakarta');
+        var age = today - guild.createdAt; // Get server Age
 
         setInterval(function(){ // Function to change it
             embedStats();
@@ -18,18 +22,20 @@ module.exports = client => {
                     let embed = new MessageEmbed()
                     .setAuthor(guild.name, guild.iconURL(({ format: 'jpg', size: 2048 })))
                     .setTitle('Server Information')
-                    .setDescription(`Welcome To ${guild.name}! This embed contains some information of the server. Before you start participating please read the rules first in <#${rulesChannelID}>. Feel free to ask <@${guild.ownerID}> or <@&${modsRolesID}> if you have any questions! Once again welcome, have fun, & please enjoy your stay ^^\n\n[Get Server Icon](${guild.iconURL(({ format: 'jpg', size: 2048 }))})`)
+                    .setDescription(`Welcome To ${guild.name}! This embed contains some information of the server. Before you start participating please read the rules first in <#${rulesChannelID}>. If you have any questions feel free to ask the owner (<@${guild.ownerID}>) or <@&${modsRolesID}>. Once again welcome, have fun, & please enjoy your stay ^^\n\n[Get Server Icon](${guild.iconURL(({ format: 'jpg', size: 2048 }))})`)
                     .setThumbnail(guild.iconURL(({ format: 'jpg', size: 2048 })))
                     .addField('Server Owner', `<@${guild.ownerID}>`, false)
                     .addField(`Rules & Guides Channel`, `<#${rulesChannelID}>`, false)
+                    .addField("Server Age", `${prettyMS(age)}`, false)
+                    .addField('Server Created At', `${Moment(guild.createdAt).tz('Asia/Jakarta').format('dddd DD MMMM YYYY HH:mm:ss')} GMT+0700`, false)
                     .addField(`Server Region`, guild.region, true)
                     .addField("Default Notification", guild.defaultMessageNotifications, true)
                     .addField("AFK Timeout", guild.afkTimeout, true)
                     .addField("Nitro/Booster", `LVL. ${guild.premiumTier}/${guild.premiumSubscriptionCount} Booster(s)`, true)
                     .addField('Total Members', guild.memberCount, true)
                     .addField('Total Bots', totalBots(), true)
-                    .addField('Status (User Only)', `**Online :** ${onlineUsers()}\n**Offline :** ${guild.memberCount - onlineUsers()}`, false)
-                    .setFooter('Last Updated at')
+                    .addField('Status (User Only)', `**Online :** ${onlineUsers()}\n**Offline :** ${(guild.memberCount - totalBots()) - onlineUsers()}`, false)
+                    .setFooter('Last Updated')
                     .setColor('RANDOM')
                     .setTimestamp();
                     
