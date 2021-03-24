66cb2bb9a841796316e1b21e454601fe27afc570
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2021-03-24 08:31:41 +0700

    turns out this could only work on local, for some reason the command does not work on hosted version

--- modules/general/commands/tool/ytdl.js
+++ modules/general/commands/tool/ytdl.js
@@ -9,7 +9,7 @@ module.exports = class extends Command {
       super('ytdl', {
         aliases: ['No alias is set for this command'],
         categories: 'tool',
-        info: 'Get download links of mp3/mp4 files of a yt link using [yt2mp3 API](https://www.yt2mp3.ws/developers/)',
+        info: 'Get download links of mp3/mp4 files of a yt link using [yt-download.org](https://www.yt2mp3.ws/developers/)',
         usage: `${prefix}command/alias <video link>`,
         guildOnly: false,
       });
@@ -43,10 +43,14 @@ module.exports = class extends Command {
 
         const msg = await message.channel.send(`Please wait... Video ID: \`${theID.replace(/&/, "")}\``);
 
-        let link = `https://www.yt-download.org/api/widget/mp3/${theID.replace(/&/, "")}`; // MP3
-        let link2 = `https://www.yt-download.org/api/widget/videos/${theID.replace(/&/, "")}` // MP4
-        let link3 = `https://www.yt-download.org/api/widget/merged/${theID.replace(/&/, "")}` // Webm/MKV
+        // let link = `https://www.yt-download.org/api/widget/mp3/${theID.replace(/&/, "")}`; // MP3
+        // let link2 = `https://www.yt-download.org/api/widget/videos/${theID.replace(/&/, "")}` // MP4
+        // let link3 = `https://www.yt-download.org/api/widget/merged/${theID.replace(/&/, "")}` // Webm/MKV
 
+        let link = `https://www.yt-download.org/file/mp3/${theID.replace(/&/, "")}`; // MP3
+        let link2 = `https://www.yt-download.org/file/mp4/${theID.replace(/&/, "")}` // MP4
+        let link3 = `https://www.yt-download.org/file/merged/${theID.replace(/&/, "")}` // Webm/MKV
+        
 
         //Fetching the HTML using axios
         const dataMp3 = await axios.get(link);
@@ -80,19 +84,27 @@ module.exports = class extends Command {
     
             // All the MP3 links
             // The 0 is the stylesheet href
-            var mp3_320KBPS = linkOnly[1].replace(/\"/g, "");
-            var mp3_256KBPS = linkOnly[2].replace(/\"/g, "");
-            var mp3_192KBPS = linkOnly[3].replace(/\"/g, "");
-            var mp3_128KBPS = linkOnly[4].replace(/\"/g, "");
+            // console.log(linkOnly)
+            // var mp3_320KBPS = linkOnly[1].replace(/\"/g, "");
+            // var mp3_256KBPS = linkOnly[2].replace(/\"/g, "");
+            // var mp3_192KBPS = linkOnly[3].replace(/\"/g, "");
+            // var mp3_128KBPS = linkOnly[4].replace(/\"/g, "");
+
+            var mp3_320KBPS = linkOnly[3].replace(/\"/g, "");
+            var mp3_256KBPS = linkOnly[4].replace(/\"/g, "");
+            var mp3_192KBPS = linkOnly[5].replace(/\"/g, "");
+            var mp3_128KBPS = linkOnly[6].replace(/\"/g, "");
     
             // Mp4
             var mp4Href = loaded[1].html().match(/href=(["'])(.*?)\1/g);
             var Mp4linkOnly = mp4Href.join("").match(/"[^"]*"/gus);
-            var onlyMp4 = Mp4linkOnly[1].replace(/\"/g, "");
+            // console.log(Mp4linkOnly);
+            var onlyMp4 = Mp4linkOnly[3].replace(/\"/g, "");
     
             // Mkv
             var mkvHref = loaded[2].html().match(/href=(["'])(.*?)\1/g);
             var mvk_WebmLinkOnly = mkvHref.join("").match(/"[^"]*"/gus);
+            // console.log(mvk_WebmLinkOnly);
 
             msg.edit(`**Loading Finished**`);
             msg.delete({
@@ -103,12 +115,12 @@ module.exports = class extends Command {
                 .setColor('RANDOM')
                 .setAuthor(`Requested by ${message.author.username}`, message.author.displayAvatarURL())
                 .setTitle(`Link Info`)
-                .setDescription(`**Video ID:** \`${theID.replace(/&/, "")}\`\n**Original Link:**\n${args[0]}`)
+                .setDescription(`**Video ID:** \`${theID.replace(/&/, "")}\`\n**Original Link:**\n${args.join(" ")}`)
                 .addField(`MP3`, `[320 Kbps](${mp3_320KBPS}) | [256Kbps](${mp3_256KBPS}) | [192Kbps](${mp3_192KBPS}) | [128Kbps](${mp3_128KBPS})`, false)
                 .addField(`MP4`, `[Quality 1](${onlyMp4}) | [More Options](${link2})`, false)
-                .addField(`Webm/Mkv`, `[Quality 1](${mvk_WebmLinkOnly[1].replace(/\"/g, "")}) | [Quality 2](${mvk_WebmLinkOnly[2].replace(/\"/g, "")}) | [Quality 3](${mvk_WebmLinkOnly[3].replace(/\"/g, "")}) | [Quality 4](${mvk_WebmLinkOnly[4].replace(/\"/g, "")}) | [More Options](${link3})`, false)
+                .addField(`Webm/Mkv`, `[Quality 1](${mvk_WebmLinkOnly[3].replace(/\"/g, "")}) | [Quality 2](${mvk_WebmLinkOnly[4].replace(/\"/g, "")}) | [Quality 3](${mvk_WebmLinkOnly[5].replace(/\"/g, "")}) | [Quality 4](${mvk_WebmLinkOnly[6].replace(/\"/g, "")}) | [More Options](${link3})`, false)
                 .addField(`To check the quality`, `You can click more options to check the quality details of the video`)
-                .setFooter(`Via yt2mp3.ws • Direct Download Link`)
+                .setFooter(`Via yt-download.org • Direct Download Link`)
                 .setColor('FF0000')
                 .setTimestamp();
                 
