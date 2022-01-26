6adc2fe0e68c3dc2cf8e5f8b16841e122c817908
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-01-26 10:38:07 +0700

    fix animemal sometimes error empty

--- modules/general/events/start.js
+++ modules/general/events/start.js
@@ -18,9 +18,9 @@ module.exports = class extends Event {
 	run(client) {
 		//Log Table to Console and login to console
 		table("notable");
+		// prettier-ignore
 		console.log(
-			`Logged in as ${client.user.tag} at ${Moment(client.readyAt).tz("Asia/Jakarta").format("dddd DD MMMM YYYY HH:mm:ss")}\nManaging ${client.guilds.cache.size} Guilds, 
-			${client.channels.cache.size} Channels, and ${client.users.cache.size} Members`
+			`Logged in as ${client.user.tag} at ${Moment(client.readyAt).tz("Asia/Jakarta").format("dddd DD MMMM YYYY HH:mm:ss")}\nManaging ${client.guilds.cache.size} Guilds, ${client.channels.cache.size} Channels, and ${client.users.cache.size} Members`
 		);
 
 		// Presence at start
