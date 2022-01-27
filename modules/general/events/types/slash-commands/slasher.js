6b6387e4945803720a3988e224f54281d012bb00
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-01-27 19:01:06 +0700

    fix register slasher

--- modules/general/events/types/slash-commands/slasher.js
+++ modules/general/events/types/slash-commands/slasher.js
@@ -1,9 +1,24 @@
 const { prefix } = require("../../../../../config");
 
 module.exports = (client) => {
+	// loop through all guilds
+	client.guilds.cache.forEach((guild) => {
+		console.log(`${guild.name} | ${guild.id}`);
+		// register slash command
+		client.api
+			.applications(client.user.id)
+			.guilds(guild.id)
+			.commands.post({
+				data: {
+					name: "prefix",
+					description: "Get bot prefix",
+				},
+			});
+	});
+
 	client.ws.on("INTERACTION_CREATE", async (interaction) => {
 		const command = interaction.data.name.toLowerCase();
-		const args = interaction.data.options;
+		// const args = interaction.data.options;
 
 		if (command == "prefix") {
 			client.api.interactions(interaction.id, interaction.token).callback.post({
