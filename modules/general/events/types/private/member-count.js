0c5e37eced82020c77cd74fbd23304ce031eac10
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2022-01-27 20:46:22 +0700

    fix member counter crash because wrong guild

--- modules/general/events/types/private/member-count.js
+++ modules/general/events/types/private/member-count.js
@@ -1,17 +1,22 @@
 module.exports = (client, guildID, channelID) => {
-	const guild = client.guilds.cache.get(guildID);
-	if (!guild) return console.log("Invalid guild for member count");
+	const personalGuild = client.guilds.cache.get(guildID);
+	if (!personalGuild) return console.log("Invalid guild for member count");
 	try {
 		const theID = channelID;
 		const updateMembers = (guild) => {
 			const channel = guild.channels.cache.get(theID);
-			channel.setName(`Total Members: ${guild.memberCount}`);
+			if (channel) channel.setName(`Total Members: ${guild.memberCount}`);
 		};
 
-		client.on("guildMemberAdd", (member) => updateMembers(member.guild));
-		client.on("guildMemberRemove", (member) => updateMembers(member.guild));
+		client.on("guildMemberAdd", (member) => {
+			// check if the guild is the personal guild
+			if (member.guild === personalGuild) updateMembers(member.guild);
+		});
+		client.on("guildMemberRemove", (member) => {
+			if (member.guild === personalGuild) updateMembers(member.guild);
+		});
 
-		updateMembers(guild);
+		updateMembers(personalGuild);
 
 		console.log(`Module: Member-Count Loaded | Loaded from local module | Now waiting for new members...`);
 	} catch (e) {
